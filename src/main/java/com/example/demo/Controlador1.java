bad6f52dbf8f38772e09089c9e3894f937091835
Alberto Díez <Alberto.Diez@bosonit.com>
2021-09-17 14:30:29 +0200

    Version1

--- src/main/java/com/example/demo/Controlador1.java
+++ src/main/java/com/example/demo/Controlador1.java
@@ -9,12 +9,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("persona")
 public class Controlador1 {
+    private boolean personaIntroducida=false;
+
+    private boolean validaPersona(Persona p){
+        if(6> p.getUsuario().length() || 10<p.getUsuario().length())
+            return false;
+        if(p.getPassword()==null || p.getName()==null || p.getCity()==null || p.getCreated_date()==null)
+            return false;
+        if(!p.getCompany_email().contains("@"))
+            return false;
+        if(!p.getPersonal_email().contains("@"))
+            return false;
+        return true;
+    }
+
+    private boolean introducirPersona(){
+        if(this.personaIntroducida)
+            return false;
+        this.personaIntroducida=true;
+        Persona p = new Persona();
+        p.setUsuario("alberto");
+        p.setPassword("aa");
+        p.setName("aa");
+        p.setCity("Zaragoza");
+        p.setCreated_date(new java.sql.Date(Calendar.getInstance().getTime().getTime()));
+        p.setCompany_email("098@09");
+        p.setPersonal_email("dsfgdfs@sdfffsd ");
+
+
+
+        if(this.validaPersona(p)) {
+            personaRepository.save(p);
+            personaRepository.flush();
+        }
+        else{
+            System.out.println("Datos de la persona NO validos");
+        }
+        return true;
+    }
 
     @Autowired
     PersonaRepository personaRepository;
@@ -28,6 +68,7 @@ public class Controlador1 {
         p.setCompany_email("A");
         personaRepository.save(p);
         personaRepository.flush();*/
+        this.introducirPersona();
         return personaRepository.findAll();
     }
 
